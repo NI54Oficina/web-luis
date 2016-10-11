ca59cf2f8a4c7ab07d558a17af15242b4afb7233
Melania Miranda <melania.smc@hotmail.com>
2016-10-11 16:56:16 -0300

    14:56 11-10-2016

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <link type="text/css" href="img/favicon.ico" rel="shortcut icon" />
+
+
+      <!--CSS-->
+      <link type="text/css"  rel="stylesheet" href="css/bootstrap.min.css">
+      <link type="text/css"  rel="stylesheet" href="css/custom-icons.css">
+
+
+      <!--JS-->
+      <script type="text/javascript" src="js/jquery.js"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+
+      <script>
+        window.console = window.console || function(t) {};
+      </script> <!-- TERMINA JS PARALLAX-->
+
+
+      <!--JS-->
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+      <script type="text/javascript" src="js/jquery.mobile-events.min.js"></script>
+
+
+
+    <!-- JS PROPIO -->
+      <script type="text/javascript" src="js/js.js "></script>
+
+    <!--CSS PROPIO-->
+      <link type="text/css" rel="stylesheet" href="css/stylesheet.css"> <!-- general / interior pagina -->
+      <link type="text/css" rel="stylesheet" href="css/xl.css"> <!-- sp -->
+      <link type="text/css"  rel="stylesheet" href="css/lg.css"> <!-- lg -->
+      <link type="text/css" rel="stylesheet" href="css/md.css"> <!-- md -->
+      <link type="text/css" rel="stylesheet" href="css/sm.css"> <!-- sm -->
+      <link type="text/css" rel="stylesheet" href="css/xs.css"> <!-- xs -->
+      <link type="text/css" rel="stylesheet" href="css/sp.css"> <!-- sp -->
+
+
+    <title></title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,7 @@
+	<?php include_once("header.php") ?>
+  <p>
+    TEST
+  </p>
+
+  </body>
+</html>
