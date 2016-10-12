91abb839e4301d6e134ac05154f6e9f5d00ca36b
Melania Miranda <melania.smc@hotmail.com>
2016-10-12 15:23:17 -0300

    15:23 12-10-2016

--- portfolio.php
+++ portfolio.php
@@ -1,32 +1,41 @@
 <?php include_once("header.php") ?>
+
 <section class="col-lg-12 col-sm-12 col-md-12 col-xs-12 portfolio">
 
+  <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 portfolio-header">
+
+    <a href="home.php">
+
+        <p class="center-to-parent border">
+          Menu
+        </p>
+    </a>
+
+  <h1><span>K</span>imena<span>S</span>td</h1>
+
+  <div class="col-lg-4 col-sm-10 col-md-4 col-xs-10 portfolio-box" >
+
+    <h2>
+      Portfolio
+    </h2>
+    <div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  </div>
 
 
-<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 portfolio-header">
-  <a href="#">Menu</a>
-<h1><span>K</span>imena<span>S</span>td</h1>
-
-<div class="">
-
-  <p>
-    <span>C</span>ontacta tu <span>K</span>i
-  </p>
-  <p>
-    <img src="" alt="" />
-    <img src="" alt="" />
-    <img src="" alt="" />
-  </p>
+  </div>
 
 
-</div>
+
+</section>
+
+<section class="col-lg-12 col-sm-12 col-md-12 col-xs-12 portfolio-2">
 
 
-</div>
-
-<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 ">
-
-  <ul class="col-lg-12 col-sm-12 col-md-12 col-xs-12 ">
+  <ul class="col-lg-12 col-sm-12 col-md-12 col-xs-12 nav-porfolio">
     <li>Todos</li>
     <li>Motion</li>
     <li>Arquitectura</li>
@@ -35,33 +44,37 @@
 
   <h2><span>A</span>nima tu <span>K</span>i</h2>
 
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
+  <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12  container-portfolio">
 
-  <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
-  <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12 ">
-    <img src="img/test.png" alt="" />
-  </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-12 col-md-4 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
 
-</div>
+        <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+        <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12 ">
+          <img src="img/test.png" alt="" />
+        </div>
+
+    </div>
+
+
 
 </section>
 </body>
