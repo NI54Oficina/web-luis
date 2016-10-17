64ec15dcc2d7b7f8d64185bd998834a8910b8250
Melania Miranda <melania.smc@hotmail.com>
2016-10-17 15:40:54 -0300

    15:40 17-10-2016

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include_once("header.php") ?>
 <body id="home">
 
-  <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 header-home" style="position:absolute; z-index:99999999">
+  <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 header-home" style="z-index:99999999">
 
     <a id="btn-menu" >
 
