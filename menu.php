94ea98c6fa217c480a4ac3dcbc40fc00fc9ede17
Mel Miranda <melania.smc@hotmail.com>
2016-10-18 20:38:18 -0300

    20:38 18-10-2016

--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
-<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12  color-menu" style="position:absolute; z-index:99999999">
+<div class="color-menu"style="position:absolute; width:auto ; padding:100px; right:0" >
 
-  <a id="btn-menu" >
+  <a id="btn-menu" class="col-lg-12 col-sm-12 col-md-12 col-xs-12" style="z-index:99999999">
+
 
         <p class="center-to-parent border button-menu ">
 
