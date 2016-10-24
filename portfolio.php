5f700c134012a1850aae464c2efd3edd3ca4ff73
Melania Miranda <melania.smc@hotmail.com>
2016-10-24 14:22:28 -0300

    14:22 24-10-2016

--- portfolio.php
+++ portfolio.php
@@ -181,7 +181,9 @@
 <section class="col-lg-12 col-sm-12 col-md-12 col-xs-12 image" >
 
 <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 img-screen">
-  <button class="border" type="button" name="button" onclick="closeGallery()">X</button>
+  <button class="border-b" type="button" name="button" onclick="closeGallery()"><p>
+    x
+  </p></button>
       <div id="img-to-display" class="">
 
       </div>
